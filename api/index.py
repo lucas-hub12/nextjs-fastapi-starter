d9fce401bd765b46a9fb5f1722c38843b593f1e7
lucas_hub12 <hyeonggyunbae@gmail.com>
2025-01-09 16:26:38 +0900

    mv api

--- api/index.py
+++ api/index.py
@@ -18,12 +18,20 @@ def age_calculator(birthday: str) -> Dict[str, str]:
     :param birthday: 생년월일 (형식: YYYY-MM-DD)
     :return: 생년월일 및 만나이를 포함한 JSON 응답
     """
-    random_age = random.randint(0, 100)
+
     today = date.today()
-    
+    birth_date =datetime.strptime(birthday, "%Y-%m-%d").date()
+
+    # 기본 나이는 올해에서 태어난 해를 뺀 값
+    age = today.year - birth_year
+
+    # 생일이 아직 오지 않았다면 나이를 1살 줄임
+    if (today.month, today.day) <= (birth_date.month, birth_date.day):
+        age = age - 1
+
     return {
             "birthday": birthday,
-            "age": str(28),
+            "age": str(age),
             "basedate": str(today),
             "message": "Age calculated successfully!"
             }
